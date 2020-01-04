2d1dd4cbe81d49d770659a0597807b376ed334fb
CHEN <david860120@gmail.com>
2020-01-04 20:22:41 +0800

    建立route controller view

---
+++ app/Http/Controllers/KaohsiungController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class KaohsiungController extends Controller
+{
+    //
+    public function index()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/taipei', 'TaipeiController@index');
+Route::get('/taichung', 'TaichungController@index');
+Route::get('/kaohsiung', 'KaohsiungController@index');
