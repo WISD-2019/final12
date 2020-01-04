5f175db4cd050ba923bcc41b0e83e3856d1cadf2
Lin Ming Ying <moon9638150@gmail.com>
2020-01-04 19:01:10 +0800

    Make models: Member, Staff, Tourcase, Tourgroup, Schedule, Reservation--all

---
+++ app/Http/Controllers/TourgroupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tourgroup;
+use Illuminate\Http\Request;
+
+class TourgroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tourgroup  $tourgroup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tourgroup $tourgroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tourgroup  $tourgroup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tourgroup $tourgroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tourgroup  $tourgroup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tourgroup $tourgroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tourgroup  $tourgroup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tourgroup $tourgroup)
+    {
+        //
+    }
+}
---
+++ database/factories/TourcaseFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Tourcase;
+use Faker\Generator as Faker;
+
+$factory->define(Tourcase::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
